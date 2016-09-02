2e1f4f81331acda21302199f5d69d3c8d92916a9
Auro Tripathy <ipserv@yahoo.com>
2016-09-01 17:21:28 -0700

    renamed run script un_bld_from_imagenet_pretrain.sh

--- run_bld_from_imagenet_pretrain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TOOLS=../../build/tools
-
-export HDF5_DISABLE_VERSION_CHECK=1
-export PYTHONPATH=.
-rm logs/bld_cnn_model.log
-GLOG_logtostderr=1  $TOOLS/caffe train -solver net/bld_cnn_solver.prototxt 2>&1 | tee -a logs/bld_cnn_no_resize_model.log
-echo "Done."
