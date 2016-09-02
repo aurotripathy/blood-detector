946d24ba1844102d4e19cd7ba7f6189ee8acba1b
Auro Tripathy <ipserv@yahoo.com>
2016-09-02 13:06:21 -0700

    playing to transfer-learning

--- run_bld_detect_from_imagenet_pretrain.sh
+++ run_bld_detect_from_imagenet_pretrain.sh
@@ -4,6 +4,15 @@ TOOLS=../../build/tools
 
 export HDF5_DISABLE_VERSION_CHECK=1
 export PYTHONPATH=.
-rm logs/bld_cnn_model.log
-GLOG_logtostderr=1  $TOOLS/caffe train -solver net/bld_cnn_solver.prototxt 2>&1 | tee -a logs/bld_cnn_no_resize_model.log
+
+# weight from alexnet
+# GLOG_logtostderr=1  $TOOLS/caffe train -solver net/bld_cnn_solver.prototxt -weights /media/tempuser/RAID\ 5/caffe-test/caffe/models/bvlc_alexnet/bvlc_alexnet.caffemodel \
+#                                        2>&1 | tee -a logs/bld_cnn_no_resize_model.log
+
+# weight from caffenet
+GLOG_logtostderr=1  $TOOLS/caffe train -solver net/bld_cnn_solver.prototxt -weights /media/tempuser/RAID\ 5/caffe-test/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+                                       2>&1 | tee -a logs/bld_cnn_no_resize_model.log
+
+
 echo "Done."
+
